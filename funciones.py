8fd35621b990ec4f539819efbd761ccf404ec2ce
Juan Eduardo <juaneduardoriva@gmail.com>
2018-08-18 15:34:45 -0300

    Varios arreglos pero queda pendiente error en linea 2 y 7.

--- funciones.py
+++ funciones.py
@@ -1,10 +1,10 @@
 def recibir_ataque(coordenada):
-    for barco in barcos:
+    for barco in barcos:  # Marca como no definida Barcos
         resultado = barco.atacar(coordenada)
         if resultado != 'a':
             break
     todos_hundidos = True
-    for barco in barcos:
+    for barco in barcos:  # Marca como no definida Barcos
         if not barco.is_hundido():
             todos_hundidos = False
     if todos_hundidos:
@@ -23,8 +23,8 @@ def imprimir_mapa(mapa):
     print("   1  2  3  4  5  6  7  8  9  10")
     print("  ------------------------------")
     for fila in mapa:
-        print(fila+"|", end='') # Aca tamien me tira un error el debugger
+        print('fila+"|", end=''')  # Aca tamien me tira un error el debugger
         for elemento in mapa[fila]:
-            print(' '+elemento+' ', end='')
+            print('elemento+' ', end=''')
         print("|")
     print("  ------------------------------")
