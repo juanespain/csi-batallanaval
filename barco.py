11febd209cfbf9eec427be988d576b6633c8e3a5
Juan Eduardo <juaneduardoriva@gmail.com>
2018-08-19 17:13:29 -0300

    Varios.

--- barco.py
+++ barco.py
@@ -1,13 +1,13 @@
 class Barco:
-    def __init__ (self,coordenadas):
+    def __init__(self, coordenadas):
         self.coordenadas_vivas = coordenadas
         self.coordenadas_tocadas = []
 
-    def atacar (self,coordenada):
+    def atacar(self, coordenada):
         if coordenada in self.coordenadas_vivas:
             self.coordenadas_vivas.remove(coordenada)
             self.coordenadas_tocadas.append(coordenada)
-            if len(self.coordenadas_vivas)==0:
+            if len(self.coordenadas_vivas) == 0:
                 return 'h'
             return 't'
         elif coordenada in self.coordenadas_tocadas:
@@ -16,7 +16,8 @@ class Barco:
 #    def print (self):
 #        print("coord vivas  :",self.coordenadas_vivas)
 #        print("coord tocadas:",self.coordenadas_tocadas)
-    def is_hundido (self):
-        if len(self.coordenadas_vivas)==0:
+
+    def is_hundido(self):
+        if len(self.coordenadas_vivas) == 0:
             return True
         return False
